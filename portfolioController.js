6e8ebac2f2eb692c1efaa0f26317f822d8783ed1
Jack Gammon <Jack Gammon>
2016-10-08 23:41:06 +1300

    Added waypoints

--- portfolioController.js
+++ portfolioController.js
@@ -1,5 +1,20 @@
-app.controller('portfolioController', function($scope){
+app.controller('portfolioController', function ($scope) {
     //$scope.x
 
     $scope.testText = "Content";
+    var waypoint = new Waypoint({
+        element: document.getElementById('section4'),
+        handler: function (direction) {
+            console.log('Scrolled to waypoint!')
+        }
+    })
+
+    // var waypoint = new Waypoint({
+    //     element: document.getElementById('section4'),
+    //     handler: function(direction){
+    //         console.log('Scrolled to waypoint!')
+    //     }
+    // });
+
+
 });
